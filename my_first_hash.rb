e01f9366ba6713695f9cc1e8c5eaec40d9c2ae20
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-03 14:54:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,13 +16,15 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+  shipping_manifest[oil paintings]
   #your code here!
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
   #your code here
   #remember to return the shipping_manifest hash
 
